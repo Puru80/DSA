feb77a95a879e4c9c7b825b81afec4a8005f288a
Puru80 <puru.agar99@gmail.com>
2023-07-11 21:31:18 +0530

    LeetCode75 - 11/7/2023 - 1

--- LeetCode75/ProductExeptItself.java
+++ LeetCode75/ProductExeptItself.java
@@ -53,20 +53,15 @@ public class ProductExeptItself {
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-
-        int product = 1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0)
-                product *= nums[i];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                res[i] = product;
-            else if (product / nums[i] < 0)
-                res[i] = 0;
-            else
-                res[i] = product / nums[i];
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
 
         return res;
