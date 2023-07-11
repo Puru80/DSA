1eab8dfe10da341913cbf491e31fda4682bb51de
Puru80 <puru.agar99@gmail.com>
2023-07-11 21:56:40 +0530

    LeetCode75 - 11/7/2023 - 2

--- LeetCode75/IncreasingTriplets.java
+++ LeetCode75/IncreasingTriplets.java
@@ -49,7 +49,13 @@ public class IncreasingTriplets{
     public static boolean increasingTriplet(int[] nums) {
         int n = nums.length;
 
-        
+        if(n < 3)
+            return false;
+
+        for(int i=1;i<n-1;i++){
+            if(nums[i-1] < nums[i] && nums[i] < nums[i+1])
+                return true;
+        }
 
         return false;
     }
@@ -64,7 +70,7 @@ public class IncreasingTriplets{
             int[] nums = Arrays.stream(input.nextLine().split(" ")).mapToInt(s -> Integer.parseInt(s))
                             .toArray();
 
-            
+            pw.println(increasingTriplet(nums));
         }
         
         pw.flush();
