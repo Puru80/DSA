0809506d0f149b32f04367dd5fd97f0914e3b2fa
Puru80 <puru.agar99@gmail.com>
2023-07-12 20:46:29 +0530

    LeetCode75 - 12/7/2023 - 1

--- LeetCode75/IncreasingTriplets.java
+++ LeetCode75/IncreasingTriplets.java
@@ -3,43 +3,47 @@ package LeetCode75;
 import java.util.*;
 import java.io.*;
 
-public class IncreasingTriplets{
+public class IncreasingTriplets {
 
-    public static PrintWriter pw ;
+    public static PrintWriter pw;
 
-    static class FastReader{
+    static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-     
-        public FastReader() throws FileNotFoundException{
+
+        public FastReader() throws FileNotFoundException {
             br = new BufferedReader(
-                new FileReader("../DSA/input.txt"));
+                    new FileReader("../DSA/input.txt"));
         }
-     
-        String next(){
+
+        String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-     
-        int nextInt() { return Integer.parseInt(next()); }
-     
-        long nextLong() { return Long.parseLong(next()); }
-     
-        double nextDouble(){ return Double.parseDouble(next()); }
-     
-        String nextLine(){
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
             String str = "";
             try {
                 str = br.readLine();
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return str;
@@ -49,30 +53,36 @@ public class IncreasingTriplets{
     public static boolean increasingTriplet(int[] nums) {
         int n = nums.length;
 
-        if(n < 3)
+        if (n < 3)
             return false;
 
-        for(int i=1;i<n-1;i++){
-            if(nums[i-1] < nums[i] && nums[i] < nums[i+1])
+        int left = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > mid)
                 return true;
+            else if (nums[i] > left && nums[i] < mid)
+                mid = nums[i];
+            else if (nums[i] < left)
+                left = nums[i];
         }
 
         return false;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         FastReader input = new FastReader();
-        pw = new PrintWriter(new
-                BufferedWriter(new FileWriter("../DSA/output.txt")));
+        pw = new PrintWriter(new BufferedWriter(new FileWriter("../DSA/output.txt")));
         int t = input.nextInt();
-        
-        while(t-->0){
+
+        while (t-- > 0) {
             int[] nums = Arrays.stream(input.nextLine().split(" ")).mapToInt(s -> Integer.parseInt(s))
-                            .toArray();
+                    .toArray();
 
             pw.println(increasingTriplet(nums));
         }
-        
+
         pw.flush();
     }
 }
